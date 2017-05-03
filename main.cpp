f3b0d86dc47e4cc808b9651763c68c7f35127224
Beznosovis <Beznosovis@mdi.ru>
2017-05-03 20:07:21 +0300

    пуллин сообщений. Видно что отображается окно. Возможно что резурсы заняты, т.к я их не выосвобождаю

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ typedef TW_UINT16 (*_DSM_Entry)(pTW_IDENTITY pOrigin,
                                TW_UINT16    MSG,
                                TW_MEMREF    pData);
 
+
 void set_capability(_DSM_Entry &pFunction, TW_IDENTITY &App, TW_IDENTITY &twIdentity) {
     TW_CAPABILITY tw_capability;
     TW_UINT16  res = pFunction(&App, &twIdentity, DG_CONTROL, DAT_CAPABILITY, MSG_GET, &tw_capability);
@@ -18,30 +19,31 @@ void set_capability(_DSM_Entry &pFunction, TW_IDENTITY &App, TW_IDENTITY &twIden
 
 }
 
-TW_UINT16 FAR PASCAL DSMCallback(pTW_IDENTITY _pOrigin,
+
+TW_UINT16 DSMCallback(pTW_IDENTITY _pOrigin,
                                  pTW_IDENTITY _pDest,
                                  TW_UINT32    _DG,
                                  TW_UINT16    _DAT,
                                  TW_UINT16    _MSG,
-                                 TW_MEMREF    _pData){}
+                                 TW_MEMREF    _pData){
+    printf("CALLBACK \n");
+}
 
-void show_ui(_DSM_Entry pFunction, TW_IDENTITY App, TW_IDENTITY twIdentity, HWND pHWND__) {
+TW_UINT16 register_callback(_DSM_Entry pFunction, TW_IDENTITY App, TW_IDENTITY twIdentity, HWND pHWND__) {
+    TW_CALLBACK callback;
+    callback.CallBackProc = (TW_MEMREF)DSMCallback;
+    callback.RefCon = 0;
+    TW_UINT16  res = pFunction(&App, &twIdentity, DG_CONTROL, DAT_CALLBACK, MSG_REGISTER_CALLBACK, (TW_MEMREF)&callback);
+    return res;
+}
+
+TW_UINT16 show_ui(_DSM_Entry pFunction, TW_IDENTITY App, TW_IDENTITY twIdentity, HWND pHWND__) {
     TW_USERINTERFACE tw_userinterface;
     tw_userinterface.ShowUI = TRUE;
     tw_userinterface.ModalUI = FALSE;
-    tw_userinterface.hParent = NULL;
+    tw_userinterface.hParent = 0;
     TW_UINT16  res = pFunction(&App, &twIdentity, DG_CONTROL, DAT_USERINTERFACE, MSG_ENABLEDS, &tw_userinterface);
-    if(TWRC_SUCCESS == res) {
-        printf("Show ui \n");
-        TW_CALLBACK callback;
-        callback.CallBackProc = (TW_MEMREF)DSMCallback;
-        callback.RefCon = 0;
-
-        res = pFunction(&App, &twIdentity, DG_CONTROL, DAT_CALLBACK, MSG_REGISTER_CALLBACK, (TW_MEMREF)&callback);
-        if(TWRC_SUCCESS == res) {
-           printf("Registered callback");
-        }
-    }
+    return res;
 }
 
 
@@ -59,7 +61,7 @@ int main() {
     lstrcpy (AppID.Manufacturer, "App's Manufacturer");
     lstrcpy (AppID.ProductFamily, "App's Product Family");
     lstrcpy (AppID.ProductName, "Specific App Product Name");
-    HMODULE hm = LoadLibrary("C:\\Users\\Monstermash\\Dropbox\\twaindsm-2.3.0.win.bin\\twain32\\TWAINDSM.dll");
+    HMODULE hm = LoadLibrary("C:\\Users\\beznosovis\\Dropbox\\twaindsm-2.3.0.win.bin\\twain32\\TWAINDSM.dll");
     if(hm == NULL) {
         printf("TWAINDSM.dll not found\n");
     } else {
@@ -78,12 +80,74 @@ int main() {
                 if(res == TWRC_CANCEL) {
                     printf("Cancel\n");
                 } else if(res == TWRC_SUCCESS) {
-                    set_capability(dsm_entry, AppID, tw_identity);
+//                    set_capability(dsm_entry, AppID, tw_identity);
                     res = dsm_entry(&AppID, NULL, DG_CONTROL, DAT_IDENTITY, MSG_OPENDS, (TW_MEMREF) &tw_identity);
                     if(res == TWRC_SUCCESS) {
                         printf("Success open DS\n");
-                        show_ui(dsm_entry, AppID, tw_identity, parent);
+//                        res = register_callback(dsm_entry, AppID, tw_identity, parent);
+                        if(res == TWRC_SUCCESS) {
+//                            printf("Success register callback\n");
+                            res = show_ui(dsm_entry, AppID, tw_identity, parent);
+                            if(res == TWRC_SUCCESS) {
+                                printf("Show ui\n");
+                                TW_UINT16  m_DSMessage = 0;
+                                TW_UINT16  twRC;
 
+                                while(!m_DSMessage)
+                                {
+                                    TW_EVENT twEvent;
+
+//                                    // If we are using callbacks, there is nothing to do here except sleep
+//                                    // and wait for our callback from the DS.  If we are not using them,
+//                                    // then we have to poll the DSM.
+//
+//                                    // Pumping messages is for Windows only
+                                    MSG Msg;
+                                    if(!GetMessage((LPMSG)&Msg, NULL, 0, 0)) {
+                                        break;
+                                    }
+
+//                                    twEvent.pEvent;
+//                                    twEvent.TWMessage;
+                                    TW_UINT16 twRC = dsm_entry(&AppID,
+                                                       &tw_identity,
+                                                       DG_CONTROL,
+                                                       DAT_EVENT,
+                                                       MSG_PROCESSEVENT,
+                                                       (TW_MEMREF)&twEvent);
+
+                                    if(twRC==TWRC_DSEVENT) {
+                                        // check for message from Source
+                                        switch (twEvent.TWMessage)
+                                        {
+                                            case MSG_XFERREADY:
+                                            case MSG_CLOSEDSREQ:
+                                            case MSG_CLOSEDSOK:
+                                            case MSG_NULL:
+//                                                gpTwainApplicationCMD->m_DSMessage = twEvent.TWMessage;
+                                                break;
+
+                                            default:
+                                                printf("Error - Unknown message in MSG_PROCESSEVENT loop\n");
+                                                break;
+                                        }
+                                    } else if(twRC == TWRC_FAILURE) {
+                                        TranslateMessage ((LPMSG)&Msg);
+                                        DispatchMessage ((LPMSG)&Msg);
+                                        TW_STATUS twStatus;
+                                        dsm_entry(&AppID, &tw_identity, DG_CONTROL, DAT_STATUS, MSG_GET, (TW_MEMREF) &twStatus);
+                                        printf(twStatus.ConditionCode + "twStatus.\n");
+                                    }
+
+                                }
+                                printf("All close\n");
+                            } else if (res == TWRC_FAILURE) {
+                                printf("Failed show ui\n");
+                                TW_STATUS twStatus;
+                                dsm_entry(&AppID, &tw_identity, DG_CONTROL, DAT_STATUS, MSG_GET, (TW_MEMREF) &twStatus);
+                                printf(twStatus.ConditionCode + "twStatus.\n");
+                            }
+                        }
                     }
                 } else if(res == TWRC_FAILURE) {
                     printf("Failure\n");
@@ -93,13 +157,6 @@ int main() {
             }
         }
     }
-    std::string input;
-//    for (;;)
-//    {
-//        std::cout << "\n(h for help) > ";
-//        std::cin >> input;
-//        std::cout << std::endl;
-//    }
     _getch();
     printf("Free library");
     FreeLibrary(hm);
